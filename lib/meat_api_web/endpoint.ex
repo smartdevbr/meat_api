47d732ae18df38b8cbbb02d07444b8b63ce668a7
Gustavo Oliveira <oliveira.gustavo@totvs.com.br>
2019-02-14 07:47:00 -0200

    created migration and model

--- lib/meat_api_web/endpoint.ex
+++ lib/meat_api_web/endpoint.ex
@@ -13,7 +13,7 @@ defmodule MeatApiWeb.Endpoint do
     at: "/",
     from: :meat_api,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w()
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
